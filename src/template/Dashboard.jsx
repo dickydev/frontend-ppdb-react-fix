7f340b8d58fcf7b241180afc08653d928c21dac2
Nath2006 <ananthapindika739@gmail.com>
2025-02-24 13:51:46 +0700

    feat(dashboard) : improve icon for better UX

--- src/template/Dashboard.jsx
+++ src/template/Dashboard.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { FaRegFileAlt, FaRegFileArchive } from 'react-icons/fa';
-import { FaHouse, FaBars, FaRegCircleUser, FaBezierCurve, FaUser, FaRegBell, FaRegMessage, FaRepeat, FaRegUser, FaArrowRightFromBracket, FaCreativeCommonsNd } from 'react-icons/fa6';
+import { FaHouse, FaBars, FaRegCircleUser, FaBezierCurve, FaUser, FaFileMedical, FaChild , FaRegMessage, FaRepeat, FaRegUser, FaArrowRightFromBracket, FaCreativeCommonsNd } from 'react-icons/fa6';
 import img from '../images/logo.png';
 import { Link } from 'react-router-dom';
 import { useNavigate } from 'react-router-dom';
@@ -10,68 +10,6 @@ const ModalProfil = ({ modal }) => {
         <div>
             
         </div>
-        // <div className={`${modal.showYourProfile ? 'flex' : 'hidden'} absolute  items-center justify-center w-full z-[99] top-0 h-screen bg-black/20`}>
-        //     <div className="max-w-2xl mx-4 mt-16 text-gray-900 bg-white rounded-lg shadow-xl sm:max-w-sm md:max-w-sm lg:max-w-sm xl:max-w-sm sm:mx-auto md:mx-auto lg:mx-auto xl:mx-auto">
-        //         <div className="h-32 overflow-hidden rounded-t-lg">
-        //             <img
-        //                 className="object-cover object-top w-full"
-        //                 src="https://images.unsplash.com/photo-1549880338-65ddcdfd017b?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        //                 alt="Mountain"
-        //             />
-        //         </div>
-        //         <div className="relative w-32 h-32 mx-auto -mt-16 overflow-hidden border-4 border-white rounded-full">
-        //             <img
-        //                 className="object-cover object-center h-32"
-        //                 src="https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=400&fit=max&ixid=eyJhcHBfaWQiOjE0NTg5fQ"
-        //                 alt="Woman looking front"
-        //             />
-        //         </div>
-        //         <div className="mt-2 text-center">
-        //             <h2 className="font-semibold">Sarah Smith</h2>
-        //             <p className="text-gray-500">Freelance Web Designer</p>
-        //         </div>
-        //         <ul className="flex items-center justify-around py-4 mt-2 text-gray-700">
-        //             <li className="flex flex-col items-center justify-around">
-        //                 <svg
-        //                     className="w-4 text-blue-900 fill-current"
-        //                     xmlns="http://www.w3.org/2000/svg"
-        //                     viewBox="0 0 20 20"
-        //                 >
-        //                     <path d="M10 15l-5.878 3.09 1.123-6.545L.489 6.91l6.572-.955L10 0l2.939 5.955 6.572.955-4.756 4.635 1.123 6.545z" />
-        //                 </svg>
-        //                 <div>2k</div>
-        //             </li>
-        //             <li className="flex flex-col items-center justify-between">
-        //                 <svg
-        //                     className="w-4 text-blue-900 fill-current"
-        //                     xmlns="http://www.w3.org/2000/svg"
-        //                     viewBox="0 0 20 20"
-        //                 >
-        //                     <path d="M7 8a4 4 0 1 1 0-8 4 4 0 0 1 0 8zm0 1c2.15 0 4.2.4 6.1 1.09L12 16h-1.25L10 20H4l-.75-4H2L.9 10.09A17.93 17.93 0 0 1 7 9zm8.31.17c1.32.18 2.59.48 3.8.92L18 16h-1.25L16 20h-3.96l.37-2h1.25l1.65-8.83zM13 0a4 4 0 1 1-1.33 7.76 5.96 5.96 0 0 0 0-7.52C12.1.1 12.53 0 13 0z" />
-        //                 </svg>
-        //                 <div>10k</div>
-        //             </li>
-        //             <li className="flex flex-col items-center justify-around">
-        //                 <svg
-        //                     className="w-4 text-blue-900 fill-current"
-        //                     xmlns="http://www.w3.org/2000/svg"
-        //                     viewBox="0 0 20 20"
-        //                 >
-        //                     <path d="M9 12H1v6a2 2 0 0 0 2 2h14a2 2 0 0 0 2-2v-6h-8v2H9v-2zm0-1H0V5c0-1.1.9-2 2-2h4V2a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v1h4a2 2 0 0 1 2 2v6h-9V9H9v2zm3-8V2H8v1h4z" />
-        //                 </svg>
-        //                 <div>15</div>
-        //             </li>
-        //         </ul>
-        //         <div className="p-4 mx-8 mt-2 border-t">
-        //             <button
-        //                 onClick={() => modal.setShowYourProfile(false)}
-        //                 className="block w-1/2 px-6 py-2 mx-auto font-semibold text-white rounded-full bg-maroon hover:shadow-lg"
-        //             >
-        //                 Close
-        //             </button>
-        //         </div>
-        //     </div>
-        // </div>
     );
 };
 
@@ -86,17 +24,11 @@ const Dashboard = ({ title, children }) => {
 
     const data = [
         { name: 'Home', ic: <FaHouse />, to: '/home' },
-        // { name: 'Struktur', ic: <FaBezierCurve />, to: '/kendaraan' },
-        { name: 'Orang Tua', ic: <FaRegCircleUser />, to: '/ortu' },
-        { name: 'Siswa', ic: <FaRegFileArchive />, to: '/siswa' },
-        { name: 'Medical', ic: <FaRegFileAlt />, to: '/medical' }
+        { name: 'Orang Tua', ic: <FaUser />, to: '/ortu' },
+        { name: 'Siswa', ic: <FaChild size={22}/>, to: '/siswa' },
+        { name: 'Medical', ic: <FaFileMedical />, to: '/medical' }
     ];
 
-    const data2 = [
-        { name: 'Home', ic: <FaHouse />, to: '/home' },
-        { name: 'Pengetahuan', ic: <FaBezierCurve />, to: '/kendaraan' },
-        { name: 'Orang Tua', ic: <FaRegCircleUser />, to: '/user' }
-    ];
 
     function tanggal() {
         const date = new Date();
