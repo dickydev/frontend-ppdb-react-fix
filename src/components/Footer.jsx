7108b494d3bbec689d5d77f39061bcfe3aff0a73
Nath2006 <ananthapindika739@gmail.com>
2025-02-24 19:48:06 +0700

    feat: improve UI design in footer by changing color to red

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,11 +5,11 @@ const Footer = () => {
     <footer className="bg-white shadow-2xl text-gray-900 py-4 rounded-xl left-0 right-0 mx-4">
       <div className="container mx-auto px-4 text-center">
         {/* Copyright Text */}
-        <p className="text-md">
+        <p className="text-md text-red-900">
           Copyright © 2025. All rights reserved LEO Group.
         </p>
         {/* Version Info */}
-        <p className="text-sm mt-1">
+        <p className="text-sm mt-1  text-red-800">
           Versi 1.0.0
         </p>
       </div>
